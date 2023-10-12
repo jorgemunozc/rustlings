696be3463b72cf6cf7452b7e573fc453eb1b25ac
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2023-10-12 15:39:29 -0300

    solved miscs exercises

--- exercises/iterators/iterators1.rs
+++ exercises/iterators/iterators1.rs
@@ -15,7 +15,7 @@
 fn main() {
     let my_fav_fruits = vec!["banana", "custard apple", "avocado", "peach", "raspberry"];
 
-    let mut my_iterable_fav_fruits = ???;   // TODO: Step 1
+    let mut my_iterable_fav_fruits = my_fav_fruits.iter();   // TODO: Step 1
 
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"banana"));
     assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: Step 2
